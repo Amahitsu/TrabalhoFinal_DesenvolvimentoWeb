67ff7d9e19350f1877ed6551b13a70cdfc523b39
AMANDA HIRATA MEDEIROS <amanda.medeiros1@alunos.sc.senac.br>
2023-11-03 23:25:12 -0300

    atualização de rotas

---
+++ src/components/Titulo.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['Geektopia'])
+
+</script>
+
+<template>
+  <h1>{{ Geektopia }}</h1>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Carrinho from './view/Carrinho.vue'
+import ConfirmarCompra from './view/ConfirmarCompra.vue'
+import DetalheProduto from './view/DetalheProduto.vue'
+import TelaPrincipal from './view/TelaPrincipal.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory} from 'vue-router'
+
+const routes = [
+    {path: '/', component: TelaPrincipal},
+    {path: '/Carrinho', component: Carrinho},
+    {path: '/ConfirmarCompra', component: ConfirmarCompra},
+    {path: '/DetalheProduto', component: DetalheProduto},
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes, 
+})
+
+createApp(App).use(router).mount('#app')
