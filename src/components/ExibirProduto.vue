ec8271accc84667aca67b71a149b95b6a4f06189
AMANDA HIRATA MEDEIROS <amanda.medeiros1@alunos.sc.senac.br>
2023-12-04 21:49:26 -0300

    upando versão antiga

--- src/components/ExibirProduto.vue
+++ src/components/ExibirProduto.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 })
 
 const produto = new Produto(props.id, props.imageTitle, props.imageUrl, props.imageAlt, null, props.produtoPreco);
-function AdicionarCarrinho(){
+function AdicionarCarrinho() {
   CarrinhoService.addProdutosCarrinho(produto)
 }
 
@@ -21,13 +21,12 @@ function AdicionarCarrinho(){
 <template>
   <div class="m-2 bg-dark rounded-3 text-white">
     <img class="img-fluid p-3" :src="imageUrl" :alt="imageAlt">
-    <span class="d-flex justify-content-center">
+    <span class="d-flex align-items-center">
       <h3 class="py-2 fs-2 fw-bolder"> {{ imageTitle }}</h3>
     </span>
     <h3 class="d-flex justify-content-center fs-3 fw-bolder"> R$ {{ produtoPreco.toFixed(2) }}</h3>
 
-    <button class="btn btn-danger btn-lg m-3" @click="() => AdicionarCarrinho()">  Comprar</button>
-
+    <button class="btn btn-danger btn-lg m-3" @click="() => AdicionarCarrinho()"> Comprar</button>
 
 
     <router-link :to="'/DetalheProduto/' + props.id">
